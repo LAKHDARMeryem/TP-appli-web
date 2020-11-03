a572aa9218be3708b408665c6ac7f3b72a1e1047
LAKHDARMeryem <72692897+LAKHDARMeryem@users.noreply.github.com>
2020-11-03 20:21:33 +0100

    .

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -3,22 +3,22 @@ function validation ( )
     document.getElementById("resultat").setAttribute("hidden",'hidden');
     document.getElementById("error").setAttribute("hidden",'hidden');
 
-    if (document.getElementById("nom").value.length < 5 && document.getElementById("nom").value.length > 0 ) { 
+    if (document.getElementById("fname").value.length < 5 && document.getElementById("fname").value.length > 0 ) { 
         document.getElementById("error").innerHTML = "Le nom doit contenir au moins 5 caractères !";
          document.getElementById("error").removeAttribute("hidden");
 	}
-	else if (document.getElementById("nom").value.length == 0 ) { 
-        document.getElementById("error").innerHTML = "La saisie du nom est obligatoire !";
+	else if (document.getElementById("fname").value.length == 0 ) { 
+        document.getElementById("error").innerHTML = "Veuillez renseigner tous les champs  !";
          document.getElementById("error").removeAttribute("hidden");
 	}
 	
 	  
-    else if (document.getElementById("prenom").value.length < 5 && document.getElementById("prenom").value.length > 0 ) { 
+    else if (document.getElementById("lname").value.length < 5 && document.getElementById("lname").value.length > 0 ) { 
         document.getElementById("error").innerHTML = "Le prénom doit contenir au moins 5 caractères !";
          document.getElementById("error").removeAttribute("hidden");
     }    
-	else if (document.getElementById("prenom").value.length == 0 ) { 
-        document.getElementById("error").innerHTML = "La saisie du prénom est obligatoire !";
+	else if (document.getElementById("lname").value.length == 0 ) { 
+        document.getElementById("error").innerHTML = "Veuillez renseigner tous les champs  !";
          document.getElementById("error").removeAttribute("hidden");
 	}
       
@@ -28,26 +28,22 @@ function validation ( )
          document.getElementById("error").removeAttribute("hidden");
     }    
 	else if (document.getElementById("adresse").value.length == 0 ) { 
-        document.getElementById("error").innerHTML = "La saisie de l'adresse est obligatoire !";
+        document.getElementById("error").innerHTML = "Veuillez renseigner tous les champs  !";
          document.getElementById("error").removeAttribute("hidden");
 	}
 	
-	      
+	
     else if (document.getElementById("mail").value.length < 5 && document.getElementById("mail").value.length > 0 ) { 
         document.getElementById("error").innerHTML = "Le mail doit contenir au moins 5 caractères !";
          document.getElementById("error").removeAttribute("hidden");
     }
 	else if (document.getElementById("mail").value.length == 0 ) { 
-        document.getElementById("error").innerHTML = "La saisie du mail est obligatoire !";
+        document.getElementById("error").innerHTML = "Veuillez renseigner tous les champs  !";
          document.getElementById("error").removeAttribute("hidden");
 	} 
 	
      else {
         document.getElementById("resultat").removeAttribute("hidden");
-        document.getElementById("resultat").innerHTML = "Bienvenue Mr " + document.querySelector("#nom").value
+        document.getElementById("resultat").innerHTML = "Bienvenue Mr / Mm" + document.querySelector("#lname").value
      }
  }
-
-
-        
-       
